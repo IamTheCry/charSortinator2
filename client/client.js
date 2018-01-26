8f636ebe970a144a4a04ce7a1ef477aab0e372b9
Corey <cry@Coreys-MacBook-Air.local>
2018-01-26 10:43:37 -0800

    Stub client behavior

---
+++ client/client.js
@@ -0,0 +1,4 @@
+// add event handler for form
+  // on enter or click, use fetch, send string
+  // on response, append to table
+  
--- server/index.js
+++ server/index.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.use(express.static('./client'))
 app.get('/', (req, res) => res.send('index.html'))
+app.get('/sort', (req, res) => res.send(// use sorter function
+))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
